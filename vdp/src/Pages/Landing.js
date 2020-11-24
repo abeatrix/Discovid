6ad796406de0d03254d889bb0956950f94ad8163
gabtorre <gtorresx3@gmail.com>
2020-11-24 10:04:35 -0800

    Merge

--- vdp/src/Pages/Landing.js
+++ vdp/src/Pages/Landing.js
@@ -6,37 +6,12 @@ import Features from "../Components/Landing/Features"
 import Past from "../Components/Landing/Past"
 import Footer from "../Components/Footer/index"
 
-
-if(!firebase.apps.length){
-    firebase.initializeApp({
-        apiKey: process.env.REACT_APP_FB_APIKey,
-        authDomain: process.env.REACT_APP_FB_AUTHD,
-        databaseURL: process.env.REACT_APP_FB_DBURL,
-        projectId: "virtualdanceparty-ff346",
-        storageBucket: process.env.REACT_APP_FB_SB,
-        messagingSenderId: process.env.REACT_APP_FB_MSID,
-        appId: process.env.REACT_APP_FB_APPID,
-        measurementId: "G-LXHE6CTR7G"
-    })
-}
-
 function Landing() {
     return (
     <>
         <Header/>
-        <NewsLetterForm/>
         <Past/>
-        <FormContainer>
-            {!confirmation ?
-            <form onSubmit={handleSubmit}>
-            <input
-                value={formValue}
-                onChange={(e) => setFormValue(e.target.value)}
-                />
-            <button type="submit">✈️</button>
-            </form> : <h1>Hi</h1>
-            }
-        </FormContainer>
+        <NewsLetterForm/>
         <Features/>
         <Footer/>
     </>
